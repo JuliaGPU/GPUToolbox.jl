0a027f56f843cea8712231a25c005599b9383f9b
Valentin Churavy <v.churavy@gmail.com>
2025-02-17 11:51:28 +0100

    Use support for gc_safe=true in Base if available

--- src/ccalls.jl
+++ src/ccalls.jl
@@ -91,13 +91,29 @@ render_arg(io, arg::Base.RefValue{T}) where {T} = print(io, "Ref{", T, "}")
 
 
 ## version of ccall that calls jl_gc_safe_enter|leave around the inner ccall
-
-# TODO: replace with JuliaLang/julia#49933 once merged
-
 # note that this is generally only safe with functions that do not call back into Julia.
 # when callbacks occur, the code should ensure the GC is not running by wrapping the code
 # in the `@gcunsafe` macro
 
+const HAS_CCALL_GCSAFE = VERSION >= v"1.13.0-DEV.70"
+
+if HAS_CCALL_GCSAFE
+    """
+        @gcsafe_ccall ...
+
+    Call a foreign function just like `@ccall`, but marking it safe for the GC to run. This is
+    useful for functions that may block, so that the GC isn't blocked from running, but may also
+    be required to prevent deadlocks (see JuliaGPU/CUDA.jl#2261).
+
+    Note that this is generally only safe with non-Julia C functions that do not call back into
+    Julia. When using callbacks, the code should make sure to transition back into GC-unsafe
+    mode using the `@gcunsafe` macro.
+    """
+    macro gcsafe_ccall(exprs)
+        exprs = pushfirst(exprs, :(gc_safe = true))
+        return Base.ccall_macro_lower((:ccall), Base.ccall_macro_parse(exprs)...)
+    end
+else
 function ccall_macro_lower(func, rettype, types, args, nreq)
     # instead of re-using ccall or Expr(:foreigncall) to perform argument conversion,
     # we need to do so ourselves in order to insert a jl_gc_safe_enter|leave
@@ -152,3 +168,4 @@ mode using the `@gcunsafe` macro.
 macro gcsafe_ccall(expr)
     return ccall_macro_lower(Base.ccall_macro_parse(expr)...)
 end
+end # HAS_CCALL_GCSAFE
