2a280de5b1887778891f59f4358215bcaf164a57
Valentin Churavy <v.churavy@gmail.com>
2025-02-17 15:26:05 +0100

    fixup! Use support for gc_safe=true in Base if available

--- src/ccalls.jl
+++ src/ccalls.jl
@@ -97,18 +97,20 @@ render_arg(io, arg::Base.RefValue{T}) where {T} = print(io, "Ref{", T, "}")
 
 const HAS_CCALL_GCSAFE = VERSION >= v"1.13.0-DEV.70"
 
+"""
+@gcsafe_ccall ...
+
+Call a foreign function just like `@ccall`, but marking it safe for the GC to run. This is
+useful for functions that may block, so that the GC isn't blocked from running, but may also
+be required to prevent deadlocks (see JuliaGPU/CUDA.jl#2261).
+
+Note that this is generally only safe with non-Julia C functions that do not call back into
+Julia. When using callbacks, the code should make sure to transition back into GC-unsafe
+mode using the `@gcunsafe` macro.
+"""
+macro gcsafe_ccall end
+
 if HAS_CCALL_GCSAFE
-    """
-        @gcsafe_ccall ...
-
-    Call a foreign function just like `@ccall`, but marking it safe for the GC to run. This is
-    useful for functions that may block, so that the GC isn't blocked from running, but may also
-    be required to prevent deadlocks (see JuliaGPU/CUDA.jl#2261).
-
-    Note that this is generally only safe with non-Julia C functions that do not call back into
-    Julia. When using callbacks, the code should make sure to transition back into GC-unsafe
-    mode using the `@gcunsafe` macro.
-    """
     macro gcsafe_ccall(exprs)
         exprs = pushfirst(exprs, :(gc_safe = true))
         return Base.ccall_macro_lower((:ccall), Base.ccall_macro_parse(exprs)...)
@@ -154,17 +156,6 @@ function ccall_macro_lower(func, rettype, types, args, nreq)
     end
 end
 
-"""
-    @gcsafe_ccall ...
-
-Call a foreign function just like `@ccall`, but marking it safe for the GC to run. This is
-useful for functions that may block, so that the GC isn't blocked from running, but may also
-be required to prevent deadlocks (see JuliaGPU/CUDA.jl#2261).
-
-Note that this is generally only safe with non-Julia C functions that do not call back into
-Julia. When using callbacks, the code should make sure to transition back into GC-unsafe
-mode using the `@gcunsafe` macro.
-"""
 macro gcsafe_ccall(expr)
     return ccall_macro_lower(Base.ccall_macro_parse(expr)...)
 end
