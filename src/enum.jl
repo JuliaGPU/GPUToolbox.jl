05626cbe63381ee1ec8e4d94fa623d0e1f5cb8d8
Tim Besard <tim.besard@gmail.com>
2026-03-24 17:31:46 +0100

    Make it possible to control the visibility of created enums. (#16)

--- src/enum.jl
+++ src/enum.jl
@@ -5,7 +5,31 @@ export @enum_without_prefix
 
 # this is useful when enum values from an underlying C library, typically prefixed for the
 # lack of namespacing in C, are to be used in Julia where we do have module namespacing.
-macro enum_without_prefix(enum, prefix)
+macro enum_without_prefix(ex...)
+    # destructure keyword arguments
+    call = ex[end-1:end]
+    kwargs = map(ex[1:end-2]) do kwarg
+        if kwarg isa Symbol
+            :($kwarg = $kwarg)
+        elseif Meta.isexpr(kwarg, :(=))
+            kwarg
+        else
+            throw(ArgumentError("Invalid keyword argument '$kwarg'"))
+        end
+    end
+
+    visibility = nothing
+    for kwarg in kwargs
+        key, val = kwarg.args
+        if key == :visibility
+            val isa QuoteNode && (val = val.value)
+            visibility = val::Symbol
+        else
+            throw(ArgumentError("Invalid keyword argument '$key'"))
+        end
+    end
+
+    enum, prefix = call
     if isa(enum, Symbol)
         mod = __module__
     elseif Meta.isexpr(enum, :(.))
@@ -21,7 +45,13 @@ macro enum_without_prefix(enum, prefix)
     for instance in instances(enum)
         name = String(Symbol(instance))
         @assert startswith(name, prefix)
-        push!(ex.args, :(const $(Symbol(name[length(prefix)+1:end])) = $(mod).$(Symbol(name))))
+        short = Symbol(name[length(prefix)+1:end])
+        push!(ex.args, :(const $short = $(mod).$(Symbol(name))))
+        if visibility == :export
+            push!(ex.args, :(export $short))
+        elseif visibility == :public
+            push!(ex.args, Expr(:public, short))
+        end
     end
 
     return esc(ex)
